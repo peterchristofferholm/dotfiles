1e0842f08edb08c6ea79a49a7ed6e44bfb33134c
Peter Christoffer Holm <phol@basecph.com>
2025-11-18 09:22:09 +0100

    Add ruff override

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -13,6 +13,18 @@ return {
         toml = { "taplo" },
         sh = { "shellcheck", "shfmt" },
       },
+      formatters = {
+        ruff_fix = {
+          prepend_args = {
+            "--config",
+            "lint.isort.relative-imports='from'",
+            "--config",
+            "lint.isort.force-single-line=false",
+            "--config",
+            "line-length=100",
+          },
+        },
+      },
       format_on_save = {
         timeout_ms = 500,
         lsp_fallback = true,
