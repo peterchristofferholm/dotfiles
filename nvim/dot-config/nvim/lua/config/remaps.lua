3d693209b052272c04ba4f956e9242be721e4b55
Peter Christoffer Holm <phol@basecph.com>
2026-03-06 10:19:48 +0100

    add double-esc to exit terminal mode

--- nvim/dot-config/nvim/lua/config/remaps.lua
+++ nvim/dot-config/nvim/lua/config/remaps.lua
@@ -24,3 +24,16 @@ vim.api.nvim_create_autocmd("User", {
 vim.keymap.set("n", "<leader>rp", function()
   vim.cmd("LspRestart pyright")
 end, { desc = "Restart Pyright LSP" })
+
+-- double escape to exit terminal mode without delaying single escape
+local esc_timer
+set("t", "<Esc>", function()
+  esc_timer = esc_timer or (vim.uv or vim.loop).new_timer()
+  if esc_timer:is_active() then
+    esc_timer:stop()
+    vim.cmd("stopinsert")
+  else
+    esc_timer:start(200, 0, function() end)
+    return "<Esc>"
+  end
+end, { expr = true, desc = "Double escape to normal mode" })
